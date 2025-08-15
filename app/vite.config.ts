e5e63a9750124d7cd7af0cb9377753a5879b44bc
Roman Martsenko <116177826+RomanBrov@users.noreply.github.com>
2025-08-15 17:59:08 +0300

    Initial commit: Telegram onboarding kit project

---
+++ app/vite.config.ts
@@ -0,0 +1,34 @@
+// app/vite.config.js
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import svgLoader from 'vite-svg-loader';
+import { telegramStickerLoader } from './_internal/tgs.loader';
+
+export default defineConfig({
+  // Якщо це репозиторна Pages:
+  // !!! заміни <repo-name> на точну назву репозиторію і залиш завершаючий слеш
+  base: '/<repo-name>/',
+  // Якщо це user/org pages або custom domain — заміни на '/'
+  // base: '/',
+
+  plugins: [
+    telegramStickerLoader(),
+    vue(),
+    svgLoader({
+      defaultImport: 'component',
+      svgoConfig: {
+        plugins: [
+          {
+            name: 'cleanupIds',
+            params: { remove: false, minify: false },
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist',        // залиш dist
+    assetsInlineLimit: 0,
+    minify: true,
+  },
+});
